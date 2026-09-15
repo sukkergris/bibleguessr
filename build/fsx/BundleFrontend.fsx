41dc83933988a66cf486aff32787098980cf906f
Theodor Heiselberg <sukkerfrit@gmail.com>
2026-09-15 12:16:48 +0000

    fix: add missing copyReleaseFiles function call in bundling script

--- build/fsx/BundleFrontend.fsx
+++ build/fsx/BundleFrontend.fsx
@@ -8,6 +8,7 @@ open System.IO.Compression
 open System.IO
 
 let rootDir = RootLoader.findRoot __SOURCE_DIRECTORY__
+
 let version = packageVersion ()
 
 let artifact = $"bibleguessr-frontend-{version}"
@@ -16,11 +17,20 @@ let artifactPath = Path.Combine( rootDir,"artifacts", artifact)
 
 let sourcePath = Path.Combine(rootDir, "frontend", "dist")
 
+let copyReleaseFiles(dist: string) =
+    let license = "LICENSE"
+    let notice = "NOTICE.md"
+    File.Copy(Path.Combine(rootDir, license), Path.Combine(sourcePath, license))
+    File.Copy( Path.Combine(rootDir, notice ), Path.Combine(sourcePath, notice))
+    ()
+
 let pack (source: string, dist: string) =
     if not (Directory.Exists source) then
         failwith $"{source} path doesn't exist"
 
     Directory.CreateDirectory(Path.GetDirectoryName dist) |> ignore
+
+    copyReleaseFiles dist
 
     ZipFile.CreateFromDirectory(source, dist + ".zip", CompressionLevel.Optimal, false)
 
